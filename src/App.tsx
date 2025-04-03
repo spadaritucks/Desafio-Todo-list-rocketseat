58db79cccdd6121915774f31f147c69827208c32
spadaritucks <thiago.spadari02@gmail.com>
2025-04-03 11:58:21 -0300

    Criação do Componente Principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
+import Header from './components/header/header'
+import Input from './components/input/input'
 
 function App() {
 
 
   return (
     <section className='main-menu'>
-      <h1>To do List</h1>
+      <Header />
+      <div className='create-task-area'>
+        <Input label='Create your task' type='text' placeholder='Add your new task' />
+        <button>Create</button>
+      </div>
     </section>
   )
 }
