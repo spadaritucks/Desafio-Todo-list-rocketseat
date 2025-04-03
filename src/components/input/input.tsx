d2570790c22f759c52a34f683133759f113683d0
spadaritucks <thiago.spadari02@gmail.com>
2025-04-03 11:59:43 -0300

    Criação do componente input personalizado

---
+++ src/components/input/input.tsx
@@ -0,0 +1,46 @@
+import './input.css'
+type InputType = "text" | "hidden" | "number" | "file"
+
+interface InputProps {
+    value?: string;
+    label: string;
+    defaultValue?: string;
+    required?: boolean
+    name?: string
+    placeholder?: string;
+    type: InputType
+    onInvalid?: () => void
+    onChange?: () => void
+    onClick?: () => void
+}
+
+
+export default function Input({
+    value,
+    label,
+    defaultValue,
+    required,
+    name,
+    type,
+    placeholder,
+    onInvalid,
+    onChange,
+    onClick }: InputProps) {
+
+    return (
+        <div className="inputWrapper">
+            <label htmlFor={label}>
+                <input
+                    defaultValue={defaultValue}
+                    required={required}
+                    type={type}
+                    name={name}
+                    value={value}
+                    placeholder={placeholder}
+                    onInvalid={onInvalid}
+                    onChange={onChange}
+                    onClick={onClick} />
+            </label>
+        </div>
+    )
+}
